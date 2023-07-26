285d9f41d69a9a1f3d5cffb7b0bfd505da849536
Walter Bivanco <walterbivanco@gmail.com>
2023-07-26 10:46:29 -0300

    Se unifico en un solo script las distintas funcionaliades de un asistente

--- app.py
+++ app.py
@@ -1,25 +1,43 @@
+# Librerías para carga de archivo .env
+from dotenv import load_dotenv
+import os
 # Librerías para Chatbot
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import openai
-from dotenv import load_dotenv
-import os
+# Librerías para convertir de texto a voz
+import pyttsx3
+import threading
+# Librerias para convertir de voz a texto
+import speech_recognition as sr
+from pydub import AudioSegment
 
 
+# Inicializar app
+app = Flask(__name__)
+#CORS(app)
+
+# Carga el API key de OpenAI
 load_dotenv()  
 openai_key = os.getenv('OPENAI_API_KEY')
-                    
-                    
-app = Flask(__name__)
-CORS(app)
 
+
+### Carga pantalla de opciones ###
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+
+### Chat ###
+# Carga pantalla de chat
+@app.route('/chat', methods=['GET'])
+def chat():
     return render_template('chat.html')
 
 
-@app.route('/api/chat', methods=['POST'])
-def chat():
+# Procesa chat
+@app.route('/chat_procesar', methods=['POST'])
+def chat_procesar():
     openai.api_key = openai_key
 
     prompt = request.json.get('message', '')
@@ -33,6 +51,51 @@ def chat():
 
     ai_message = response['choices'][0]['text']
     return jsonify({'message': ai_message})
+
+
+### Texto a voz ###
+# Carga pantalla de ingreso de texto
+@app.route("/hablar")
+def hablar():
+    return render_template("convertir_tav.html")
+
+
+@app.route("/speak", methods=["POST"])
+def speak():
+    text = request.form.get("text")
+    threading.Thread(target=speak_async, args=(text,)).start()
+    return jsonify(success=True)
+
+# Procesa el texto ingresado
+def speak_async(text):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 125)
+    engine.setProperty("voice", "spanish")
+    engine.say(text)
+    engine.runAndWait()
+
+
+### Voz a texto ###
+# Transcribe el audio ingresado
+@app.route('/transcribir', methods=['GET', 'POST'])
+def transcribir():
+    transcript = ""
+
+    if request.method == 'POST':
+        audio_file = request.files['file']
+        audio_file.save("audio.wav")
+        audio = AudioSegment.from_wav("audio.wav")
+
+        recognizer = sr.Recognizer()
+        with sr.AudioFile("audio.wav") as source:
+            audio_data = recognizer.record(source)
+            try:
+                transcript = recognizer.recognize_google(audio_data, language='es-ES')
+            except Exception as e:
+                print(e)
+                transcript = "Lo siento, no pude transcribir el audio."
+
+    return render_template('convertir_vat.html', transcript=transcript)
 
 
 if __name__ == '__main__':
